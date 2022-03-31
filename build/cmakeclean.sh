ea804645290a4c013099b2ab6501c9cee8106f10
Matt Norman <normanmr@ornl.gov>
2022-03-31 13:55:39 -0400

    Reorg of dynamics source files

--- build/cmakeclean.sh
+++ build/cmakeclean.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 rm -rf CMakeCache.txt  CMakeFiles  cmake_install.cmake  CTestTestfile.cmake  Makefile  yakl  driver  \
-       core  micro  modules
+       core  micro  modules  dynamics
 
