8b3643a63a6ad44a0b4635cfa9e9ec579e75bafa
Matt Norman <normanmr@ornl.gov>
2022-04-06 14:22:23 -0400

    Fix to column forcing in MPI context

--- model/modules/column_nudging.h
+++ model/modules/column_nudging.h
@@ -32,12 +32,7 @@ namespace modules {
       state.add_field( dm.get<real const,3>("temp"       ) );
       state.add_field( dm.get<real const,3>("water_vapor") );
 
-      YAKL_SCOPE( column , this->column );
-
-      real factor = 1._fp / (nx*ny);
-      parallel_for( Bounds<4>(num_fields,nz,ny,nx) , YAKL_LAMBDA (int l, int k, int j, int i) {
-        yakl::atomicAdd( column(l,k) , state(l,k,j,i)*factor );
-      });
+      column = get_column_average( coupler , state );
     }
 
 
@@ -58,12 +53,7 @@ namespace modules {
       state.add_field( dm.get<real,3>("temp"       ) );
       state.add_field( dm.get<real,3>("water_vapor") );
 
-      real2d state_col_avg("state_col_avg",num_fields,nz);
-      yakl::memset(state_col_avg,0._fp);
-      real factor = 1._fp / (nx*ny);
-      parallel_for( Bounds<4>(num_fields,nz,ny,nx) , YAKL_LAMBDA (int l, int k, int j, int i) {
-        yakl::atomicAdd( state_col_avg(l,k) , state(l,k,j,i)*factor );
-      });
+      auto state_col_avg = get_column_average( coupler , state );
 
       YAKL_SCOPE( column , this->column );
 
@@ -72,6 +62,38 @@ namespace modules {
         state(l,k,j,i) += dt * ( column(l,k) - state_col_avg(l,k) ) / time_scale;
       });
     }
+
+
+    template <class T>
+    real2d get_column_average( core::Coupler const &coupler , core::MultiField<T,3> &state ) const {
+      using yakl::c::parallel_for;
+      using yakl::c::Bounds;
+
+      int nz = coupler.get_nz();
+      int ny = coupler.get_ny();
+      int nx = coupler.get_nx();
+
+      real2d column_loc("column_loc",num_fields,nz);
+      yakl::memset( column_loc , 0._fp );
+
+      real factor = 1._fp / (nx*ny);
+      parallel_for( Bounds<4>(num_fields,nz,ny,nx) , YAKL_LAMBDA (int l, int k, int j, int i) {
+        yakl::atomicAdd( column_loc(l,k) , state(l,k,j,i)*factor );
+      });
+
+      realHost2d column_total_host("column_total_host",num_fields,nz);
+      MPI_Allreduce( column_loc.createHostCopy().data() , column_total_host.data() , num_fields*nz ,
+                     MPI_DOUBLE , MPI_SUM , MPI_COMM_WORLD );
+      
+      auto column_total = column_total_host.createDeviceCopy();
+      int nranks = coupler.get_nranks();
+      parallel_for( Bounds<2>(num_fields,nz) , YAKL_LAMBDA (int l, int k) {
+        column_loc(l,k) = column_total(l,k) / nranks;
+      });
+
+      return column_loc;
+    }
+
   };
 
 }
