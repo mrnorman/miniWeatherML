b6ea978d628371b56f3a3eb8df5e8b79488426ad
Matt Norman <normanmr@ornl.gov>
2022-12-06 19:50:43 -0500

    Getting rid of MW_ORD in summit machine file. Fixing bug in sponge layer that didn't average across tasks.

--- model/modules/sponge_layer.h
+++ model/modules/sponge_layer.h
@@ -23,18 +23,6 @@ namespace modules {
     std::vector<std::string> tracer_names = coupler.get_tracer_names();
     int num_tracers = coupler.get_num_tracers();
 
-    // Allocate MultiField of all state and tracer havg variables, since we're doing the same operation on each
-    core::MultiField<real,1> havg_fields;
-    havg_fields.add_field( real1d("havg_rho_d",nz) );
-    havg_fields.add_field( real1d("havg_uvel" ,nz) );
-    havg_fields.add_field( real1d("havg_vvel" ,nz) );
-    havg_fields.add_field( real1d("havg_wvel" ,nz) );
-    havg_fields.add_field( real1d("havg_temp" ,nz) );
-    for (int tr=0; tr < num_tracers; tr++) {
-      char const * name = (std::string("havg_")+tracer_names[tr]).c_str();
-      havg_fields.add_field( real1d(name,nz) );
-    }
-
     auto &dm = coupler.get_data_manager_readwrite();
 
     // Create MultiField of all state and tracer full variables, since we're doing the same operation on each
@@ -48,21 +36,24 @@ namespace modules {
       full_fields.add_field( dm.get<real,3>(tracer_names[tr]) );
     }
 
-    int num_fields = havg_fields.get_num_fields();
-
-    real r_nx_ny = 1._fp / (nx*ny);
-
-    // Zero out the havg_fields
-    parallel_for( YAKL_AUTO_LABEL() , Bounds<2>(num_fields,nz) , YAKL_LAMBDA (int ifld, int k) {
-      havg_fields(ifld,k) = 0;
-    });
+    int num_fields = full_fields.get_num_fields();
 
     // Compute the horizontal average for each vertical level (that we use for the sponge layer)
-    parallel_for( YAKL_AUTO_LABEL() , Bounds<4>(num_fields,num_layers,ny,nx) ,
-                  YAKL_LAMBDA (int ifld, int kloc, int j, int i) {
+    real2d havg_fields("havg_fields",num_fields,num_layers);
+    havg_fields = 0;
+    real r_nx_ny = 1._fp / (nx*ny);
+    parallel_for( YAKL_AUTO_LABEL() , Bounds<4>(num_fields,num_layers,ny,nx) , YAKL_LAMBDA (int ifld, int kloc, int j, int i) {
       int k = nz - 1 - kloc;
-      if (ifld != WFLD) yakl::atomicAdd( havg_fields(ifld,k) , full_fields(ifld,k,j,i) * r_nx_ny );
+      if (ifld != WFLD) yakl::atomicAdd( havg_fields(ifld,kloc) , full_fields(ifld,k,j,i) * r_nx_ny );
     });
+
+    auto havg_fields_loc_host = havg_fields.createHostCopy();
+    auto havg_fields_host = havg_fields_loc_host.createHostObject();
+    MPI_Allreduce( havg_fields_loc_host.data() , havg_fields_host.data() , havg_fields_host.size() , MPI_DOUBLE , MPI_SUM , MPI_COMM_WORLD );
+    havg_fields_host.deep_copy_to(havg_fields);  // After this, havg_fields has the sum, not average, over tasks
+
+    int num_tasks;
+    MPI_Comm_size( MPI_COMM_WORLD , &num_tasks );
 
     real constexpr time_scale = 60;  // strength of each application is dt / time_scale  (same as SAM's tau_min)
     real time_factor = dt / time_scale;
@@ -74,7 +65,7 @@ namespace modules {
       real rel_dist = ( zlen - z ) / ( num_layers * dz );
       real space_factor = ( cos(M_PI*rel_dist) + 1 ) / 2;
       real factor = space_factor * time_factor;
-      full_fields(ifld,k,j,i) += ( havg_fields(ifld,k) - full_fields(ifld,k,j,i) ) * factor;
+      full_fields(ifld,k,j,i) += ( havg_fields(ifld,kloc)/num_tasks - full_fields(ifld,k,j,i) ) * factor;
     });
   }
 
