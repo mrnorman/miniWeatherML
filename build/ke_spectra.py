4faa72e0b39a20ea50fd8bd07529f2018fce120a
Matt Norman <normanmr@ornl.gov>
2022-12-08 19:20:28 -0500

    Updating ke_spectra script with improved moving average operation

--- build/ke_spectra.py
+++ build/ke_spectra.py
@@ -4,17 +4,7 @@ import pandas as pd
 import numpy as np
 
 def rm(x,N):
-  ret = x
-  h = int(np.floor(N/2))
-  nx = x.shape[0]
-  for i in range(nx) :
-    if (i-h<0) :
-      ret[i] = np.average(x[0:i+1])
-    elif (i-h+N>nx-1) :
-      ret[i] = np.average(x[i:nx])
-    else :
-      ret[i] = np.average(x[i-h:i-h+N])
-  return ret
+  return np.convolve(x, np.ones(N), 'valid') / N
 
 
 def ke_spectra(u,v,w) :
@@ -41,19 +31,16 @@ def ke_spectra(u,v,w) :
   return freq2d , spd
     
 
+avg_size = 50
+
 nc = Dataset("test.nc", "r")
 u = nc.variables["uvel"][-1,:,:,:]
 v = nc.variables["vvel"][-1,:,:,:]
 w = nc.variables["wvel"][-1,:,:,:]
 freq,spd = ke_spectra(u,v,w)
+plt.loglog( rm(freq,avg_size) , rm(spd,avg_size) )
 
-plt.loglog( freq , rm(spd,20) )
-
-example = 1.e-1 * freq**(-5./3.)
-plt.loglog( freq , example )
-
-example = 2.e-4 * freq[int(len(freq)*1/2):]**(-6.)
-plt.loglog( freq[int(len(freq)*1/2):] , example )
+plt.loglog( rm(freq,avg_size) , 1.e-1 * rm(freq,avg_size)**(-5./3.) )
 
 #plt.xlim([4.e-2,freq[-1]])
 #plt.ylim()
