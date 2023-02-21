f2a347a98119430fb3489273d443a7a3b81e6662
Matt Norman <normanmr@ornl.gov>
2023-02-20 22:18:37 -0500

    Sponge layer appears to work now

--- model/modules/sponge_layer.h
+++ model/modules/sponge_layer.h
@@ -9,11 +9,14 @@ namespace modules {
     using yakl::c::parallel_for;
     using yakl::c::Bounds;
 
-    int  nz   = coupler.get_nz  ();
-    int  ny   = coupler.get_ny  ();
-    int  nx   = coupler.get_nx  ();
-    real zlen = coupler.get_zlen();
-    real dz   = coupler.get_dz  ();
+    auto ny_glob = coupler.get_ny_glob();
+    auto nx_glob = coupler.get_nx_glob();
+    auto nz      = coupler.get_nz  ();
+    auto ny      = coupler.get_ny  ();
+    auto nx      = coupler.get_nx  ();
+    auto nens    = coupler.get_nens();
+    auto zlen    = coupler.get_zlen();
+    auto dz      = coupler.get_dz  ();
 
     int num_layers = 10;  // Number of model top vertical layers that participate in the sponge relaxation
 
@@ -26,25 +29,25 @@ namespace modules {
     auto &dm = coupler.get_data_manager_readwrite();
 
     // Create MultiField of all state and tracer full variables, since we're doing the same operation on each
-    core::MultiField<real,3> full_fields;
-    full_fields.add_field( dm.get<real,3>("density_dry") );
-    full_fields.add_field( dm.get<real,3>("uvel"       ) );
-    full_fields.add_field( dm.get<real,3>("vvel"       ) );
-    full_fields.add_field( dm.get<real,3>("wvel"       ) );
-    full_fields.add_field( dm.get<real,3>("temp"       ) );
+    core::MultiField<real,4> full_fields;
+    full_fields.add_field( dm.get<real,4>("density_dry") );
+    full_fields.add_field( dm.get<real,4>("uvel"       ) );
+    full_fields.add_field( dm.get<real,4>("vvel"       ) );
+    full_fields.add_field( dm.get<real,4>("wvel"       ) );
+    full_fields.add_field( dm.get<real,4>("temp"       ) );
     for (int tr=0; tr < num_tracers; tr++) {
-      full_fields.add_field( dm.get<real,3>(tracer_names[tr]) );
+      full_fields.add_field( dm.get<real,4>(tracer_names[tr]) );
     }
 
     int num_fields = full_fields.get_num_fields();
 
     // Compute the horizontal average for each vertical level (that we use for the sponge layer)
-    real2d havg_fields("havg_fields",num_fields,num_layers);
+    real3d havg_fields("havg_fields",num_fields,num_layers,nens);
     havg_fields = 0;
-    real r_nx_ny = 1._fp / (nx*ny);
-    parallel_for( YAKL_AUTO_LABEL() , Bounds<4>(num_fields,num_layers,ny,nx) , YAKL_LAMBDA (int ifld, int kloc, int j, int i) {
+    parallel_for( YAKL_AUTO_LABEL() , Bounds<5>(num_fields,num_layers,ny,nx,nens) ,
+                                      YAKL_LAMBDA (int ifld, int kloc, int j, int i, int iens) {
       int k = nz - 1 - kloc;
-      if (ifld != WFLD) yakl::atomicAdd( havg_fields(ifld,kloc) , full_fields(ifld,k,j,i) * r_nx_ny );
+      if (ifld != WFLD) yakl::atomicAdd( havg_fields(ifld,kloc,iens) , full_fields(ifld,k,j,i,iens) );
     });
 
     auto havg_fields_loc_host = havg_fields.createHostCopy();
@@ -53,19 +56,17 @@ namespace modules {
     MPI_Allreduce( havg_fields_loc_host.data() , havg_fields_host.data() , havg_fields_host.size() , mpi_data_type , MPI_SUM , MPI_COMM_WORLD );
     havg_fields_host.deep_copy_to(havg_fields);  // After this, havg_fields has the sum, not average, over tasks
 
-    int num_tasks;
-    MPI_Comm_size( MPI_COMM_WORLD , &num_tasks );
-
     real time_factor = dt / time_scale;
 
     // use a cosine relaxation in space:  ((cos(pi*rel_dist)+1)/2)^2
-    parallel_for( YAKL_AUTO_LABEL() , Bounds<4>(num_fields,num_layers,ny,nx) , YAKL_LAMBDA (int ifld, int kloc, int j, int i) {
+    parallel_for( YAKL_AUTO_LABEL() , Bounds<5>(num_fields,num_layers,ny,nx,nens) ,
+                                      YAKL_LAMBDA (int ifld, int kloc, int j, int i, int iens) {
       int k = nz - 1 - kloc;
       real z = (k+0.5_fp)*dz;
       real rel_dist = ( zlen - z ) / ( num_layers * dz );
       real space_factor = ( cos(M_PI*rel_dist) + 1 ) / 2;
       real factor = space_factor * time_factor;
-      full_fields(ifld,k,j,i) += ( havg_fields(ifld,kloc)/num_tasks - full_fields(ifld,k,j,i) ) * factor;
+      full_fields(ifld,k,j,i,iens) += ( havg_fields(ifld,kloc,iens)/(nx_glob*ny_glob) - full_fields(ifld,k,j,i,iens) ) * factor;
     });
   }
 
